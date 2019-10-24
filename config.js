d325f905e685c34eb9b821a60062b5720712dd09
Sandra Tsao <sandratsao@Sandras-MBP.fc.ca>
2019-10-24 12:38:05 -0400

    class file

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port: 8889, // 8889 for mac, 3306 for win
+    uname: "root",
+    pword: "root", // leave blank for windows
+    database: "db_week7"
+}
+
+module.exports = config;
